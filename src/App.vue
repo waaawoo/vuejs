760ab364e9c4cb3a85917b998b79a225799433d7
kazuya-minatani <waaawooo37ta2@gmail.com>
2021-04-08 00:05:56 +0900

    コンポーネントをグローバル、ローカルで使用する

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,20 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <LikeHeader></LikeHeader>
+    <LikeNumber></LikeNumber>
   </div>
 </template>
 
+
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// ローカルで使うコンポーネントをインポートする
+import LikeHeader from "./LikeHeader";
 
 export default {
-  name: 'App',
+  // コンポーネントを用意
   components: {
-    HelloWorld
-  }
-}
+    // 使いたい名前: コンポーネントの中身
+    LikeHeader,
+  },
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 import Vue from 'vue'
+// コンポーネントオブジェクトを呼び出している
 import App from './App.vue'
+import LikeNumber from "./LikeNumber.vue"
 
 Vue.config.productionTip = false
+
+// グローバルインポートしたファイルのメソッドを呼び出し
+Vue.component("LikeNumber", LikeNumber);
 
 new Vue({
   render: h => h(App),
